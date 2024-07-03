66543f2ba833ff6abdc52a6f29d45f47f72ac000
Prateek Verma <prateek222rithik@gmail.com>
2024-07-03 20:25:13 +0530

    updates

--- String/src/Palindrome.java
+++ String/src/Palindrome.java
@@ -1,7 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Palindrome {
+public class Palindrome {   //a b c d c b a
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String str= in.next();
