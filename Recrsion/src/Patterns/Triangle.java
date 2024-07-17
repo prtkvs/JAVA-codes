1a85157101c487b7bf01633dbea3150691eb8db6
Prateek Verma <prateek222rithik@gmail.com>
2024-07-17 19:03:50 +0530

    patterns

---
+++ Recrsion/src/Patterns/Triangle.java
@@ -0,0 +1,33 @@
+package Patterns;
+
+public class Triangle {
+    public static void main(String[] args) {
+        triangle2(7,0);      //maximum rows and column start
+    }
+    static void triangle(int r , int c){
+        if (r==0){
+            return ;
+        }
+        if (c<r){
+            System.out.print("*");
+            triangle(r,c+1);
+        }
+        else{
+            System.out.println();
+            triangle(r-1,0);    //col starts from zero
+        }
+    }
+    static void triangle2(int r , int c){
+        if (r==0){
+            return ;
+        }
+        if (c<r){
+            triangle2(r,c+1);
+            System.out.print("*");
+        }
+        else{
+            triangle2(r-1,0);    //col starts from zero
+            System.out.println();
+        }
+    }
+}
