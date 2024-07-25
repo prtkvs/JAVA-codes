7cec5d54c0a8261d4aa4ac91381fefae17fe80b2
Prateek Verma <prateek222rithik@gmail.com>
2024-07-25 16:33:13 +0530

    recursion done

--- Recrsion/src/SubsetSubsequenceString/skip.java
+++ Recrsion/src/SubsetSubsequenceString/skip.java
@@ -2,32 +2,56 @@ package SubsetSubsequenceString;
 
 public class skip {
     public static void main(String[] args) {
-        skip("","baccadh");
-        System.out.println(skip2("baccadah"));
+        System.out.println(skipAppNotApple("bacapplcdah"));
     }
-    static void skip(String p,String up){  //p=process,up= unprocessed
-     if (up.isEmpty()){
-         System.out.println(p);
-         return;
-     }
-     char ch = up.charAt(0);
 
-     if (ch=='a'){
-         skip(p,up.substring(1));  //check notes about substring here 1(inclusive) from index to last print e.g "hello".substring(1) = "ello"
-     }else{
-         skip(p+ch,up.substring(1));  //substring used here to start again from next index after checking 0th index
-     }
-    }
-    static String skip2(String up){
-        if (up.isEmpty()){
-            return ""; //check notes
+    static void skip(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
         }
         char ch = up.charAt(0);
 
-        if (ch=='a'){
-            return skip2(up.substring(1));
-        }else {
-            return ch + skip2(up.substring(1));
+        if (ch == 'a') {
+            skip(p, up.substring(1));
+        } else {
+            skip(p + ch, up.substring(1));
+        }
+    }
+
+    static String skip(String up) {
+        if (up.isEmpty()) {
+            return "";
+        }
+
+        char ch = up.charAt(0);
+
+        if (ch == 'a') {
+            return skip(up.substring(1));
+        } else {
+            return ch + skip(up.substring(1));
+        }
+    }
+
+    static String skipApple(String up) {
+        if (up.isEmpty()) {
+            return "";
+        }
+        if (up.startsWith("apple")) {
+            return skipApple(up.substring(5));
+        } else {
+            return up.charAt(0) + skipApple(up.substring(1));
+        }
+    }
+
+    static String skipAppNotApple(String up) {
+        if (up.isEmpty()) {
+            return "";
+        }
+        if (up.startsWith("app") && !up.startsWith("apple")) {
+            return skipAppNotApple(up.substring(3));
+        } else {
+            return up.charAt(0) + skipAppNotApple(up.substring(1));
         }
     }
 }
