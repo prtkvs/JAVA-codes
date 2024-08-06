a3e9189112b4e4b895e68e176602885ad5a0c501
Prateek Verma <prateek222rithik@gmail.com>
2024-08-06 23:48:25 +0530

    knapsack

--- DP/src/Knapsack/knapsack01.java
+++ DP/src/Knapsack/knapsack01.java
@@ -2,17 +2,25 @@ package Knapsack;
 
 public class knapsack01 {
     public static void main(String[] args) {
-        int [] val = {15,14,22,34,16}
-        int [] wt = {1,5,2,3,6};
+        int[] val = {15, 14, 10, 45, 30};
+        int[] wt = {1, 5, 1, 3, 4};
         int W = 7;
+        System.out.println(knapsack(val, wt, W, val.length));
     }
-    static int knapsack(int[] val,int[] wt,int W ,int n){ //no. of the item we're talking about
-        if (n==0 || W ==0){
+
+    public static int knapsack(int[] val, int[] wt, int W, int n) { //no. of the item we're talking about
+        if (n == 0 || W == 0) {
             return 0;
         }
-    //now since first item is stored at 0th index
-        if (wt[n-1]<=W){
-
+        //now since first item is stored at 0th index
+        if (wt[n - 1] <= W) {    // valid
+            // include
+            int ans1 = val[n - 1] + knapsack(val, wt, W - wt[n - 1], n - 1);
+            // exclude
+            int ans2 = knapsack(val, wt, W, n - 1);
+            return Math.max(ans1, ans2);
         }
+        // not valid (exclude)
+        return knapsack(val, wt, W, n - 1);
     }
 }
