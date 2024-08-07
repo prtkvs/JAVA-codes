6374c65125294dda483fbd93a34ad0e4bbfbdef3
Prateek Verma <prateek222rithik@gmail.com>
2024-08-07 19:49:24 +0530

    targetSumSubset

---
+++ DP/src/Knapsack/targetSum.java
@@ -0,0 +1,41 @@
+package Knapsack;
+
+public class targetSum {
+    public static boolean targetSumSubset(int[] arr,int sum){
+        int n = arr.length;     // no. of items
+        boolean[][] dp = new boolean[n+1][sum+1];
+        //initializing
+        for (int i = 0; i < n+1; i++) {
+            dp[i][0] = true;
+        }
+        for (int i=1; i<n+1 ; i++){
+            for (int j = 0; j < sum+1; j++) {
+                int v = arr[i-1]; // value
+                // include
+                if (v<=j && dp[i-1][j-v] == true ){
+                    dp[i][j] = true;
+                }
+                //exclude
+                if (dp[i-1][j] == true){
+                    dp[i][j] = true;
+                }
+            }
+        }
+        print(dp);
+        return dp[n][sum];
+    }
+    public static void print(boolean[][] dp){   // called in knapsackTab
+        System.out.println("DP array is :- ");
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j <dp[0].length ; j++) {
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr ={4,2,7,1,3};
+        System.out.println(targetSumSubset(arr,10));
+    }
+}
