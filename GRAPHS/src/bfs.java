cfdb37aeff06de43343915a8c8e986a6fd4e933b
Prateek Verma <prateek222rithik@gmail.com>
2024-09-10 18:49:06 +0530

    files arranged

--- GRAPHS/src/bfs.java
+++ GRAPHS/src/bfs.java
@@ -1,68 +1,77 @@
-import java.util.ArrayList;
 import java.util.*;
+
 public class bfs {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
 
-    public class BFS {
-        static class Edge{      // we made this static to call this in psvm
-            int src;
-            int dest;
-            int wt;
-
-            public Edge(int src, int dest, int wt) {
-                this.src = src;
-                this.dest = dest;
-                this.wt = wt;
-            }
-        }
-
-
-        static void createGraph(ArrayList <Edge>[]graph){
-            for (int i = 0; i < graph.length; i++) {
-                graph[i] = new ArrayList<>();
-            }
-
-            //adding edges info at vertices
-            //0 index
-            graph[0].add(new Edge(0,1,1));
-
-            // 1 index
-            graph[1].add(new Edge(1,0,5));
-            graph[1].add(new Edge(1,3,1));
-
-            //2 index
-            graph[2].add(new Edge(2,0,1));
-            graph[2].add(new Edge(2,4,1));
-
-            //3 index
-            graph[3].add(new Edge(3,2,1));
-            graph[3].add(new Edge(3,4,1));
-            graph[3].add(new Edge(3,5,1));
-
-            //4 index
-            graph[4].add(new Edge(4,2,1));
-            graph[4].add(new Edge(4,3,1));
-            graph[4].add(new Edge(4,5,1));
-
-        }
-        public static void bfs(ArrayList<Edge>[] graph){
-            Queue<Integer> q = new LinkedList<>();
-            boolean[] vis = new boolean[graph.length];
-            q.add(0);   // source: 0
-            if (!q.isEmpty()){
-                int curr = q.remove();
-
-                if(!vis[curr]){
-                    System.out.println(curr+" ");   // visit current
-                    vis[curr]=true;
-                    //visiting neighbours code (covered)
-                    
-                }
-            }
-        }
-        public static void main(String[] args) {
-            int V =7;
-            ArrayList<Edge> graph[] = new ArrayList[V];
-            createGraph(graph);
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
         }
     }
+
+    static void createGraph(ArrayList<Edge> graph[]){
+        for(int i = 0; i<graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge( 0,  1,  1));
+        graph[0].add(new Edge( 0, 2,  1));
+
+        graph[1].add(new Edge( 1, 0,  1));
+        graph[1].add(new Edge( 1, 3 , 1));
+
+        graph[2].add(new Edge( 2,  0,  1));
+        graph[2].add(new Edge( 2,  4,  1));
+
+        graph[3].add(new Edge( 3,  1,  1));
+        graph[3].add(new Edge( 3,  4,  1));
+        graph[3].add(new Edge( 3,  5,  1));
+
+        graph[4].add(new Edge( 4,  2,  1));
+        graph[4].add(new Edge( 4,  3,  1));
+        graph[4].add(new Edge( 4,  5,  1));
+
+        graph[5].add(new Edge( 5,  6,  1));
+        graph[5].add(new Edge( 5,  3,  1));
+        graph[5].add(new Edge( 5,  4,  1));
+
+
+    }
+
+    public static void bfs(ArrayList<Edge> graph[]){
+        Queue <Integer> q = new LinkedList<>(); // queue to store elements
+        boolean vis[] = new boolean[graph.length]; // visited array
+
+        q.add(0); // source 0 as we are starting from 0;
+
+        while(!q.isEmpty()){ //while q is not empty
+            int curr = q.remove();
+
+            if(!vis[curr]){ // if the element is not visited
+                System.out.print(curr+" "); //printing
+                vis[curr] = true; // make it visited true;
+                for(int i =0; i<graph[curr].size(); i++){
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+
+
+            }
+        }
+
+    }
+
+    public static void main(String args[]){
+
+        int V = 7;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+
+        bfs(graph);
+    }
+
 }
--- GRAPHS/src/minDistance/allPaths.java
+++ GRAPHS/src/minDistance/allPaths.java
@@ -1,3 +1,5 @@
+package minDistance;
+
 import java.util.*;
 
 public class allPaths {
