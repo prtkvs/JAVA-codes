84bcd8669ac4cff200382caed0c38e07a34f9d2f
Prateek Verma <prateek222rithik@gmail.com>
2025-06-27 16:18:52 +0530

    custom stack

--- StacknQueues/src/CustomStack.java
+++ StacknQueues/src/CustomStack.java
@@ -1,20 +1,20 @@
-public class CustomStack {
-    protected int[] data;
+//Custom Stack Implementation
+public class CustomStack{
+    int[] data ; // stored in an array
     private static final int DEFAULT_SIZE = 10;
-
+    //if size not mentioned use DEFAULT_SIZE
     public CustomStack(){
         this(DEFAULT_SIZE);
     }
-
-    public CustomStack(int size) {
+    //if size is mentioned then use it for data array
+    public CustomStack(int size){
         this.data = new int[size];
     }
-
     int ptr = -1;
 
-    public boolean push(int item) {
-        if (isFull()) {
-            System.out.println("Stack is full!!");
+    //add items - push
+    public boolean push(int item){
+        if(isFull()){
             return false;
         }
         ptr++;
@@ -22,28 +22,62 @@ public class CustomStack {
         return true;
     }
 
-    public int pop() throws StackException {
-        if (isEmpty()) {
-            throw new StackException("Cannot pop from an empty stack!!");
+    //remove items - pop
+    public int pop() throws Exception{
+        if(isEmpty()){
+            throw new Exception("Cannot pop from an empty stack");
         }
-//        int removed = data[ptr];
-//        ptr--;
-//        return removed;
-        return data[ptr--];
+        // return data[ptr--]; //LIFO returns data[ptr] then ptr--
+        int removed = data[ptr];
+        ptr--;
+        return removed;
     }
 
-    public int peek() throws StackException {
-        if (isEmpty()) {
-            throw new StackException("Cannot peek from an empty stack!!");
+    //peek
+    public int peek() throws Exception{
+        if(isEmpty()){
+            throw new Exception("Cannot peek from an empty stack");
         }
-        return data[ptr];
+        return data[ptr]; // the one's which'll pop first
     }
 
-    public boolean isFull() {
-        return ptr == data.length - 1; // ptr is at last index
+    //ifFull
+    public boolean isFull(){
+        // if(ptr==data.length-1){
+        //     return true;
+        // }
+        // return false;
+        return ptr==data.length-1;
     }
 
-    public boolean isEmpty() {
+    //isEmpty
+    public boolean isEmpty(){
+        // if(ptr==-1){
+        //     return true;
+        // }
+        // return false;
         return ptr == -1;
     }
+
+    //Display - got stucked
+    public void display(){
+        for (int i=0;i<=ptr ;i++ ){
+            System.out.print(data[i]+"->");
+        }
+        System.out.print("END");
+    }
+
+
+//    public static void main (String[] args) throws Exception {
+//        CustomStack stack = new CustomStack(6);
+//        stack.push(1);
+//        stack.push(2);
+//        stack.push(3);
+//        stack.push(4);
+//        stack.push(5);
+//        stack.push(6);
+//        System.out.println(stack.pop());
+//        System.out.println(stack.peek());
+//        stack.display();
+//    }
 }
