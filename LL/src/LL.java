f951724f67cbc5f48aeae645f1b14e7e7a8ba41f
Prateek Verma <prateek222rithik@gmail.com>
2025-06-27 16:16:47 +0530

    LL updated

--- LL/src/LL.java
+++ LL/src/LL.java
@@ -1,150 +1,132 @@
-public class LL {
-    private class Node{
-        private int value;
-        private Node next;
-
-        public Node(int value) {
-            this.value = value;
+public class LL{
+    class Node{
+        Node next;
+        int val;
+        public Node(Node next, int val){
+            this.val=val;
+            this.next=next;
         }
-
-        public Node(int value, Node next) {   //overloading of Node
-            this.value = value;
-            this.next = next;
+        public Node(int val){
+            this.val=val;
         }
     }
     private Node head;
     private Node tail;
     int size;
 
-    public LL() {
-        this.size = 0;
+    public LL(){
+        this.size=0; //declare size first
     }
 
-    public void insertFirst(int value) {
-        Node node = new Node(value);
-        node.next = head;
-        head = node;
-        if (tail == null) {        //only 1 node = head =tail
-            tail = head;
+    //InsertFirst
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head; // assigning
+        head = node; //validating
+        //null ptr Exception
+        if (tail == null){
+            tail = head; //assign that new head value to tail also
         }
         size++;
     }
 
-    public void insertLast(int value) {
-        if (tail == null) {
-            insertFirst(value);
-
-        } else {
-            Node node = new Node(value);
-            tail.next = node;
-            tail = node;
-            size++;
+    //InsertLast
+    public void insertLast(int val){
+        if (tail == null){
+            insertFirst(val);
         }
-    }
-
-    public void indexInsert(int value, int index) {      //since we have value and to place at index + we have already made a constructor which takes two value at start
-        if (index == 0) {
-            insertFirst(value);
-        }
-        if (index == size) {
-            insertLast(value);
-        }
-        Node temp = head;  //already know that head cannot be moved this way till the index so usko temp mein store karke chalao.
-        for (int i = 1; i < index; i++) {   //i.e if the index is 3 , you have to go till 2  and starting from 1 since 0 is head itself from where temp is starting
-            temp = temp.next;    //reaches till 2nd index
-        }
-        Node node = new Node(value, temp.next);   //create that node to insert in between. There the value is something e.g 7 and temp.next to connect it with next one(3rd index) 7->8(now 4th index)
-        temp.next = node;                        //assigning 3rd index to 7 (9->7->8)
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
         size++;
     }
-//    public Node find(int value){
-//        Node node = head;                         //gives a pointer to a before index
-//        while(node != null) {
-//            if (node.value == value){
-//                return node;
-//            }
-//            node = node.next;
-//        }
-//        return null;
-//    }
 
-    // insertion using recursion
-//    public void insertRec(int val , int index){
-//        head = insertRec(val, index, head);
-//    }
-//    private Node insertRec(int val , int index , Node node){
-//        if (index==0){  // for adding an element at an index
-//            Node temp = new Node(val,node);
-//            size++;
-//            return temp;
-//        }
-//        node.next = insertRec(val,index-- ,node.next); // traversing backwards
-//        return node;
-//    }
-    public void insertRec(int val, int index) {
-        head = insertRec(val, index, head);
-    }
-    private Node insertRec(int val, int index, Node node) {
-        if (index == 0) {
-            Node temp = new Node(val, node);
-            size++;
-            return temp;
+    //InsertIndex
+    public void insertIndex(int val, int index){
+        //two conditions based on index position
+        if(index==0){
+            insertFirst(val);
         }
-
-        node.next = insertRec(val, index-1, node.next);
-        return node;
+        if(index == size-1){
+            insertLast(val);
+        }
+        Node temp = head;
+        for (int i=1;i<index ;i++ ){ // 1 se start in insert and 0 se start in Delete
+            temp = temp.next; //index se ek pehle tk
+        }
+        Node node = new Node(temp.next,val);
+        //socho...socho...socho  ? DOUBT - done (good)
+        temp.next = node; // good
+        // node = temp.next.next;  //doubt line -> no need of this
+        size++;
     }
 
-    public int deleteFirst() {
-        int value = head.value;
+    //Delete first
+    public void deleteFirst(){
+        if(head == null){
+            return;
+        }
         head = head.next;
-        if (head == null) {
-            tail = null;
-        }
         size--;
-        return value;
     }
 
-    public int deleteLast() {
-        if (size <= 1) {    //null items
-            return deleteLast();
-        }
-        Node secondLast = get(size - 2);    //get method below
-        int value = tail.value;
-        tail = secondLast;      //since putting secondLast as tail not tail as secondLast
-        tail.next = null;
-        size--;
-        return value;
-    }
-
-    public Node get(int index) {            //for deleting last
+    //Delete Last -> size-2 = secondLast concept
+    // get function O(n)
+    public Node get(int index){
         Node node = head;
-        for (int i = 0; i < index; i++) {     //zero since node is an item of LL
+        for (int i=0;i<index ;i++ ){
             node = node.next;
         }
         return node;
     }
-    public int deleteIndex(int index){
-        if (index==0){
-            return deleteFirst();
+
+    public void deleteLast(){
+        if(size<=1){ //Doubt in null ptr Exception - corrected
+            return;
         }
-        if (index==size-1){
-            return deleteLast();
-        }
-        Node prev = get(index-1);          //index-1 becoz previous is behind that index i.e to be deleted
-        int value = prev.next.value;   //not prev.value.next since you're not taking the value and doing next rather you're taking next one's value.
-        //now to break the older chain
-        prev.next= prev.next.next;
+        Node secondLast = get(size-2);
+        tail = secondLast;
+        tail.next = null; // p+p+p
         size--;
-        return value;
     }
+
+    //Delete Index -> prev concept
+    public void deleteIndex(int index){
+        // was doubt in null ptr Exception - use insertIndex concept
+        if(index==0){
+            deleteFirst();
+        }
+        if(index == size-1){
+            deleteLast();
+        }
+        Node prev = get(index-1);
+        prev.next = prev.next.next;
+        size--;
+    }
+    //Display -> while loop
     public void display(){
         Node temp = head;
-        while ( temp != null){
-            System.out.print(temp.value+" -> ");
-            temp= temp.next;
+        while(temp!=null){
+            System.out.print(temp.val+" -> ");
+            temp = temp.next;
         }
-        System.out.println("END");
+        System.out.print("END");
     }
+//    public static void main (String[] args) {
+//        LL list = new LL();
+//        list.insertFirst(21);
+//        list.insertFirst(19);
+//        list.insertFirst(17);
+//        list.insertFirst(13);
+//        list.insertFirst(11);
+//        list.insertFirst(7);
+//        list.insertFirst(5);
+//        list.insertLast(23);
+//        list.insertIndex(15,4); //val,index
+//        list.deleteFirst();
+//        list.deleteLast(); // problem -> corrected
+//        list.deleteIndex(4); // problem -> corrected
+//        list.insertIndex(17,4);
+//        list.display();
+//    }
 }
-
