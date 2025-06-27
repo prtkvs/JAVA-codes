2dcf516b4f763fe94fd3b87f222b43be915e3747
Prateek Verma <prateek222rithik@gmail.com>
2025-06-27 16:21:21 +0530

    circular queue

--- StacknQueues/src/CircularQueue.java
+++ StacknQueues/src/CircularQueue.java
@@ -1,67 +1,75 @@
-public class CircularQueue {
-    protected int[] data;
+public class CircularQueue{
+    int[] data ;
     private static final int DEFAULT_SIZE = 10;
 
-    protected int end = 0;
-    protected int front = 0;
-    private int end_ptr = 0;
-
     public CircularQueue(){
-        this(DEFAULT_SIZE);
+        this(DEFAULT_SIZE); // mtlb kuch size nhi mentioned to direct use this
     }
-
-    public CircularQueue(int size) {
+    public CircularQueue(int size){
         this.data = new int[size];
     }
+    int first =0;
+    int end = 0;
+    int end_ptr =0;
 
-    public boolean isFull() {
-        return end_ptr == data.length; // ptr is at last index
+    public boolean isFull(){
+        return end_ptr == data.length;
     }
-
-    public boolean isEmpty() {
+    public boolean isEmpty(){
         return end_ptr == 0;
     }
 
-    public boolean insert(int item) {
-        if (isFull()) {
+    //add items - end badhega
+    public boolean insert(int item){
+        if (isFull()){
             return false;
         }
         data[end++] = item;
-        end = end % data.length;
+        end = end%data.length;
         end_ptr++;
         return true;
     }
-
-    public int remove() throws Exception {
-        if (isEmpty()) {
-            throw new Exception("Queue is empty");
+    // remove items = first ko badhao
+    public int remove() throws Exception{
+        if(isEmpty()){
+            throw new Exception("Can't remove from empty queue");
         }
-
-        int removed = data[front++];
-        front = front % data.length;
+        int removed = data[0];
+        first++;
+        first= first%data.length;
         end_ptr--;
         return removed;
     }
-
-    public int front() throws Exception{
-        if (isEmpty()) {
-            throw new Exception("Queue is empty");
+    // offer item
+    public int offer() throws Exception{
+        if(isEmpty()){
+            throw new Exception("Can't offer from empty queue");
         }
-        return data[front];
+        return data[first]; //doubt- cleared
     }
 
-    public void display() {
-        if (isEmpty()) {
-            System.out.println("Empty");
-            return;
+    public void display(){ //empty case lelo aur do while loop use
+        if(isEmpty()){
+            System.out.print("Empty Queue");
         }
-        int i = front;
-        do {
-            System.out.print(data[i] + " -> ");
+        int i = first; // doubt : cleared - first se end show krna hai
+        do{
+            System.out.print(data[i]+" -> ");
             i++;
-            i %= data.length;
-        } while (i != end);
+            i=i%data.length;
+        }while(i!=end);  // equality doubt
         System.out.println("END");
     }
-
+//    public static void main (String[] args) throws Exception {
+//        CircularQueue cq = new CircularQueue(10);
+//        cq.insert(1);
+//        cq.insert(2);
+//        cq.insert(3);
+//        cq.insert(4);
+//        cq.insert(5);
+//        cq.insert(6);
+//        cq.remove();
+//        cq.display();
+//        System.out.println(cq.offer());
+//    }
 }
